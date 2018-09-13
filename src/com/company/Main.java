9af4841befbdc1b1413eae3b7e41cf9e05ff7826
Tanner Wildasin <tw073@pvsd.org>
2018-09-13 11:14:25 -0400

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner kbin = new Scanner(System.in);
+        System.out.println("Enter an Integer:");
+        int in = kbin.nextInt();
+        System.out.println(((in % 2) == 0) ? "The integer \"" + in + "\" is even." : "The integer \"" + in + "\" is odd." );
+
     }
 }
